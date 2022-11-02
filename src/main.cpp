04c4470377acae6a26ff2ccda73baa15c0a8f137
Lafisl <117240892+Lafisl@users.noreply.github.com>
2022-11-02 08:32:56 +0700

    proper files

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       e2421428                                                  */
+/*    Created:      Tue Nov 01 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    20, 10, 1, 11   
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
